a6b13dbe561fcb58920529732ae4f07d61422c59
nathanrobertbaldwin <nathanrobertbaldwin@gmail.com>
2023-10-10 12:20:48 -0500

    Converting types.

--- src/data_types/scalar_types.rs
+++ src/data_types/scalar_types.rs
@@ -1,7 +1,23 @@
 pub fn add_five (num: i32 ) -> i32 {
-    return num + 5;
+    let ans = num + 5;
+    println!("Adding {} + 5 = {}", num, ans);
+    return ans;
 }
 
 pub fn add_floats (num1:f64, num2:f64) -> f64 {
-    return num1 + num2;
+    let ans: f64 =  num1 + num2;
+    println!("Adding {} + {} = {}", num1, num2, ans);
+    return ans;
+}
+
+pub fn convert_int (num1: i32) -> f64 {
+    let float: f64 = num1.into();
+    println!("Converted num {} into {}", num1, format!("{:.10}", float));
+    return float;
+}
+
+pub fn char_code (letter: char) -> u32 {
+    let ucode: u32 = letter.into();
+    println!("The character code for {} is {}", letter, ucode);
+    return ucode;
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod data_types;
+use data_types::scalar_types;
 
 fn main() {
-    println!("{}", data_types::scalar_types::add_five(2));
-    println!("{}", data_types::scalar_types::add_floats(2.88, -0.282828));
+    scalar_types::add_five(2);
+    scalar_types::add_floats(2.991, -8.222);
+    scalar_types::convert_int(3);
+    scalar_types::char_code('A');
 }
